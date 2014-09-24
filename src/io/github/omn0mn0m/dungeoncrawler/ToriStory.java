d74fcef91ffa5bae2ef6cba9484094f7f4b9a170
omn0mn0m <tranngocnam97@yahoo.com>
2014-09-23 21:55:59 -0400

    Started Tori's story intro

---
+++ src/io/github/omn0mn0m/dungeoncrawler/ToriStory.java
@@ -0,0 +1,16 @@
+package io.github.omn0mn0m.dungeoncrawler;
+
+import io.github.omn0mn0m.util.Input;
+
+public class ToriStory
+{
+	private Input input = new Input();
+	
+	public ToriStory() {
+		
+	}
+	
+	public void runStory() {
+		System.out.println("Hey Tori, this is Nam's disembodied voice!");
+	}
+}
